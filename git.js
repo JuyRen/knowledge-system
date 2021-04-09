7b74bca1c222cca48e047f78f12679b3be57df2a
renjunyang <renjunyang@auraxy.com>
2021-04-09 23:28:52 +0800

    dev:5

--- git.js
+++ git.js
@@ -5,7 +5,7 @@ console.log("master-1");
 // 2. 1 master操作-2
 console.log("master-2");
 // 3. develop操作-1
-console.log("develop-1");
+console.log("develop-2");
 // 3.1 develop操作-2
 // 3.2 develop操作-3
 // 3.3 develop操作-4
